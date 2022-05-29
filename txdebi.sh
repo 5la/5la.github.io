e2124e049c6ec75b0b9f2cb146432113ebca1f0c
5la <86772550+5la@users.noreply.github.com>
2022-05-29 16:57:34 +0800

    Update txdebi.sh

--- txdebi.sh
+++ txdebi.sh
@@ -247,8 +247,8 @@ while [ $# -gt 0 ]; do
             ;;
         --china)
             dns='119.29.29.29 180.76.76.76'
-            mirror_protocol=http
-            mirror_host=mirrors.tencentyun.com
+            mirror_protocol=https
+            mirror_host=mirrors.tencent.com
             ntp=ntp.tencent.com
             security_repository=mirror
             ;;
